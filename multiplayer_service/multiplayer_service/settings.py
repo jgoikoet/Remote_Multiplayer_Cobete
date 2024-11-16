f606da099345863d29f9e0963521418f05f13709
aitorfi <aitorfidalgo@gmail.com>
2024-11-16 14:57:08 +0100

    Match hiostory views implemented

--- multiplayer_service/multiplayer_service/settings.py
+++ multiplayer_service/multiplayer_service/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'channels',
+	'corsheaders',
 	'game'
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://127.0.0.1:49999",
+    "https://localhost:49999",
+	"https://127.0.0.1:49998",
+    "https://localhost:49998"
+]
+CORS_ORIGIN_ALLOW_ALL = True 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    "https://127.0.0.1:49999",
+    "https://localhost:49999",
+	"https://127.0.0.1:49998",
+    "https://localhost:49998"
+]
+CORS_ALLOW_ALL_ORIGINS = True
